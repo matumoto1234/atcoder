29f705260c61b2f83c19da1779d95f0015921e5b
matumoto1234 <55180183+matumoto1234@users.noreply.github.com>
2021-06-21 10:25:59 +0900

    Delete abc/abc50~99 directory

--- abc/abc50~99/abc052/abc052_A.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-int main(){
-  int a,b,c,d;
-  cin>>a>>b>>c>>d;
-  cout<<max(a*b,c*d)<<endl;
-  return 0;
-}
--- abc/abc50~99/abc099/abc099_A.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-using ll = long long;
-int main(){
-  ll n;
-  cin>>n;
-  cout<<(n<=999?"ABC":"ABD")<<endl;
-  return 0;
-}
