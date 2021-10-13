f789d42dcab717e9e62fcad08f317e6050d7c4cb
matumoto1234 <matumotosizuoka@gmail.com>
2021-10-13 19:25:49 +0900

    solve up typical 001

--- typical90/001.cpp
+++ typical90/001.cpp
@@ -1,34 +1,97 @@
+#if !__INCLUDE_LEVEL__
+#include __FILE__
+
+
+
+
+int main() {
+  int n,l,k;
+  cin>>n>>l>>k;
+  vector<int> as(n);
+  cin>>as;
+
+  ll valid=0;
+  ll invalid=INF32;
+  auto is_valid = [&](ll m){
+    ll last=0;
+    int cnt=0;
+    range(i,len(as)){
+      if(as[i]-last>=m){
+        last=as[i];
+        cnt++;
+        if(cnt>=k) break;
+      }
+    }
+    return l-last>=m and cnt>=k;
+  };
+  while(abs(valid-invalid)>1){
+    ll mid=(valid+invalid)/2;
+    if(is_valid(mid)) valid=mid;
+    else invalid=mid;
+  }
+  cout<<valid<<endl;
+}
+
+
+
+
+
+
+
+#else
 #include <bits/stdc++.h>
 using namespace std;
 
+// {{{
+
 // clang-format off
-#define rep(i, n) for (int i = 0; i < (int)(n); i += 1)
-#define rrep(i, n) for (int i = (int)(n)-1; i >= 0; i -= 1)
-#define range(i, l, r) for ( int i = (int)(l); i < (int)(r); (i) += 1 )
-#define rrange(i, l, r) for ( int i = (int)(r)-1; i >= (int)(l); (i) -= 1 )
-#define debug(...) debug_func(#__VA_ARGS__, __VA_ARGS__)
-template <typename T, typename... T2> void debug_func(string_view name, const T &a, T2 &&...rest) { stack<char> bs; string_view lbs = "({[<"; string_view rbs = ")}]>"; int end = name.size(); for ( int i = 0; i < (int)name.size(); i++ ) { if ( lbs.find(name[i]) != string::npos ) { bs.push(name[i]); } if ( rbs.find(name[i]) != string::npos ) { if ( !bs.empty() ) { bs.pop(); } } if ( name[i] == ',' && bs.empty() ) { end = i; break; } } cerr << name.substr(0, end) << ":" << a; if constexpr ( sizeof...(rest) == 0 ) { cerr << endl; } else { cerr << ' '; debug_func(name.substr(name.find_first_not_of(' ', end + 1)), forward<T2>(rest)...); } }
+#define len(x) (static_cast<int>((x).size()))
+#define _over_load(_1,_2,_3,_4,NAME,...) NAME
+#define range(...) _over_load(__VA_ARGS__, range4, range3, range2)(__VA_ARGS__)
+#define range2(i, r) for ( int i = 0; i < static_cast<int>(r); (i) += 1)
+#define range3(i, l, r) for ( int i = static_cast<int>(l); i < static_cast<int>(r); (i) += 1)
+#define range4(i, l, r, step) for ( int i = static_cast<int>(l); i < static_cast<int>(r); (i) += (step))
+#define rrange(...) _over_load(__VA_ARGS__, rrange4, rrange3, rrange2)(__VA_ARGS__)
+#define rrange2(i, r) for ( int i = static_cast<int>(r) - 1; i >= 0; (i) -= 1)
+#define rrange3(i, l, r) for ( int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= 1)
+#define rrange4(i, l, r, step) for ( int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= (step))
+#define whole(f, x, ...) ([&](decltype((x)) container) { return (f)( begin(container), end(container), ## __VA_ARGS__); })(x)
+#define rwhole(f, x, ...) ([&](decltype((x)) container) { return (f)( rbegin(container), rend(container), ## __VA_ARGS__); })(x)
+#define debug(...) debug_function(#__VA_ARGS__, __VA_ARGS__)
+template <typename T, typename... T2> void debug_function(string_view name, const T &a, T2 &&...rest) {
+  stack<char> bs;
+  string_view lbs = "({[<", rbs = ")}]>";
+  int end = name.size();
+  for ( int i = 0; i < len(name); i++ ) {
+    if ( lbs.find(name[i]) != string::npos ) bs.push(name[i]);
+    if ( rbs.find(name[i]) != string::npos and !bs.empty() ) bs.pop();
+    if ( name[i] == ',' and bs.empty() ) {
+      end = i;
+      break;
+    }
+  }
+  cerr << name.substr(0, end) << ":" << a;
+  if constexpr ( sizeof...(rest) == 0 ) {
+    cerr << '\n';
+  } else {
+    cerr << ' ';
+    debug_function(name.substr(name.find_first_not_of(' ', end + 1)), forward<T2>(rest)...);
+  }
+}
 template <typename T> vector<T> make_vector(size_t a, T b) { return vector<T>(a, b); }
 template <typename... Ts> auto make_vector(size_t a, Ts... ts) { return vector<decltype(make_vector(ts...))>(a, make_vector(ts...)); }
-template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
-template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) { return a > b && (a = b, true); }
+template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) { return a < b and (a = b, true); }
+template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) { return a > b and (a = b, true); }
 template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &p) { os << p.first << ' ' << p.second; return os; }
-template <typename T1, typename T2> ostream &operator<<(ostream &os, const map<T1, T2> &v) { for ( pair<T1, T2> x : v ) { os << x << "\n"; } return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const map<T1, T2> &v) { for ( pair<T1, T2> x : v ) { os << "(" << x.first <<", " << x.second << ")" << (v.rbegin()->first == x.first ? "" : ", "); } return os; }
 template <typename T> ostream &operator<<(ostream &os, queue<T> v) { if(!v.empty()) { os << v.front(); v.pop(); } while (!v.empty()) { os << " " << v.front(); v.pop(); } return os; }
 template <typename T> ostream &operator<<(ostream &os, stack<T> v) { if(!v.empty()) { os << v.top(); v.pop(); } while (!v.empty()) { os << " " << v.top(); v.pop(); } return os; }
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
-template <typename T> ostream &operator<<(ostream &os, const deque<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { range(i, len(v)) { cout << v[i] << (i == len(v) - 1 ? "" : " "); } return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &v) { range(i, len(v)) { cout << v[i] << (i == len(v) - 1 ? "" : " "); } return os; }
 template <typename T> ostream &operator<<(ostream &os, const set<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
-template <typename T> void operator+=(vector<T> &vs, T x) { for ( T &v : vs ) v += x; }
-template <typename T> void operator-=(vector<T> &vs, T x) { for ( T &v : vs ) v -= x; }
-template <typename T> void operator*=(vector<T> &vs, T x) { for ( T &v : vs ) v *= x; }
-template <typename T> void operator/=(vector<T> &vs, T x) { for ( T &v : vs ) v /= x; }
-template <typename T> void operator%=(vector<T> &vs, T x) { for ( T &v : vs ) v %= x; }
-template <typename T> vector<T> operator+(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v += x; return res; }
-template <typename T> vector<T> operator-(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v -= x; return res; }
-template <typename T> vector<T> operator*(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v *= x; return res; }
-template <typename T> vector<T> operator/(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v /= x; return res; }
-template <typename T> vector<T> operator%(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v %= x; return res; }
+template <typename T1, typename T2> istream &operator>>(istream &is, pair<T1, T2> &p) { is >> p.first >> p.second; return is; }
+template <typename T> istream &operator>>(istream &is, vector<T> &v) { for (T &in : v) is >> in; return is; }
+struct IoSetup { IoSetup(int x = 15) { cin.tie(nullptr); ios::sync_with_stdio(false); cout << fixed << setprecision(x); cerr << fixed << setprecision(x); } } iosetup;
 using ull = unsigned long long;
 using ll = long long;
 using Pll = pair<ll, ll>;
@@ -38,44 +101,11 @@ constexpr int INF32 = INT32_MAX / 2;
 constexpr int dy[] = { 0, -1, 1, 0, -1, 1, -1, 1 };
 constexpr int dx[] = { -1, 0, 0, 1, -1, -1, 1, 1 };
 constexpr int mod998244353 = 998244353;
-constexpr int mod1000000007 = (int)1e9 + 7;
+constexpr int mod1000000007 = static_cast<int>(1e9) + 7;
 constexpr char newl = '\n';
 // clang-format on
 
+// }}}
 
 
-int main() {
-  cin.tie(nullptr);
-  ios::sync_with_stdio(false);
-
-
-  int n,l,k;
-  cin>>n>>l>>k;
-  vector<int> a(n);
-  for(auto &A:a)cin>>A;
-  
-  ll valid=-1;
-  ll invalid=l+1;
-  auto is_valid = [&](ll m){
-    // 最大何個の「長さM以上のピース」に分割できるかを解く
-    // それがK＋１以上であればOK
-    ll rem=l,now=0,pre=0;
-    int ans=1;
-    rep(i,n){
-      now=a[i]-pre;
-      rem=l-a[i];
-      if(now>=m && rem>=m){
-        now=0;
-        pre=a[i];
-        ans++;
-      }
-    }
-    return ans>=k+1;
-  };
-  while(abs(valid-invalid)>1){
-    ll mid=(valid+invalid)/2;
-    if(is_valid(mid)) valid=mid;
-    else invalid=mid;
-  }
-  cout<<valid<<endl;
-}
+#endif
